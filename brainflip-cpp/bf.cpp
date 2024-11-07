63c615ebabeb76b2a76823e4ae1bfd9827b35175
Brensen Villegas <brensen11@gmail.com>
2024-11-06 23:01:17 -0700

    seems to be working, fixed a bug where I was reading tape index as i8 not i32 oops

--- brainflip-cpp/bf.cpp
+++ brainflip-cpp/bf.cpp
@@ -9,6 +9,7 @@
 #include <map>
 #include <string>
 #include <memory>
+#include <stack>
 
 using namespace llvm;
 
@@ -17,28 +18,38 @@ static std::unique_ptr<IRBuilder<>> Builder;
 static std::unique_ptr<Module> TheModule;
 static std::map<std::string, Value *> NamedValues;
 
+Value* getTapeIndex(GlobalVariable* tapeIndex) {
+    return Builder->CreateLoad(Type::getInt32Ty(*TheContext), tapeIndex, "currentIndex");
+}
+
+Value* getTapePtr(ArrayType* TapeType, GlobalVariable* Tape, Value* currentIndex) {
+    return Builder->CreateGEP(TapeType, Tape, {ConstantInt::get(Type::getInt8Ty(*TheContext), 0), currentIndex}, "tapePtr");
+}
+
 void compile(std::string program)
 {
     TheContext = std::make_unique<LLVMContext>();
     TheModule = std::make_unique<Module>("my cool bfcompiler", *TheContext);
     Builder = std::make_unique<IRBuilder<>>(*TheContext);
 
-    ArrayType *TapeType = ArrayType::get(Type::getInt8Ty(*TheContext), 1024 * 1024 * 4);
-    GlobalVariable *Tape = new GlobalVariable(*TheModule, TapeType, false, GlobalValue::PrivateLinkage,
+    ArrayType* TapeType = ArrayType::get(Type::getInt8Ty(*TheContext), 1024 * 1024 * 64);
+    GlobalVariable* Tape = new GlobalVariable(*TheModule, TapeType, false, GlobalValue::PrivateLinkage,
                                               Constant::getNullValue(TapeType), "tape");
 
-    GlobalVariable *tapeIndex = new GlobalVariable(*TheModule, Type::getInt32Ty(*TheContext), false, GlobalValue::PrivateLinkage,
-                                                   Constant::getNullValue(Type::getInt32Ty(*TheContext)), "tapePointer");
+    GlobalVariable* tapeIndex = new GlobalVariable(*TheModule, Type::getInt32Ty(*TheContext), false, GlobalValue::PrivateLinkage,
+                                                   ConstantInt::get(Type::getInt32Ty(*TheContext), (1024 * 1024 * 64) / 2), "tapePointer");
 
-    FunctionType *putcharType = FunctionType::get(Type::getInt32Ty(*TheContext), {Type::getInt32Ty(*TheContext)}, false);
-    Function *putcharFunc = Function::Create(putcharType, Function::ExternalLinkage, "my_putchar", TheModule.get());
+    std::stack<std::pair<BasicBlock*, BasicBlock*>> loopStack;
 
-    FunctionType *getcharType = FunctionType::get(Type::getInt32Ty(*TheContext), {}, false);
-    Function *getcharFunc = Function::Create(getcharType, Function::ExternalLinkage, "my_getchar", TheModule.get());
+    FunctionType* putcharType = FunctionType::get(Type::getInt32Ty(*TheContext), {Type::getInt32Ty(*TheContext)}, false);
+    Function* putcharFunc = Function::Create(putcharType, Function::ExternalLinkage, "my_putchar", TheModule.get());
 
-    FunctionType *mainFuncType = FunctionType::get(Type::getInt32Ty(*TheContext), false);
-    Function *mainFunction = Function::Create(mainFuncType, Function::ExternalLinkage, "main", TheModule.get());
-    BasicBlock *entryBlock = BasicBlock::Create(*TheContext, "entry", mainFunction);
+    FunctionType* getcharType = FunctionType::get(Type::getInt32Ty(*TheContext), {}, false);
+    Function* getcharFunc = Function::Create(getcharType, Function::ExternalLinkage, "my_getchar", TheModule.get());
+
+    FunctionType* mainFuncType = FunctionType::get(Type::getInt32Ty(*TheContext), false);
+    Function* mainFunction = Function::Create(mainFuncType, Function::ExternalLinkage, "main", TheModule.get());
+    BasicBlock* entryBlock = BasicBlock::Create(*TheContext, "entry", mainFunction);
     Builder->SetInsertPoint(entryBlock);
 
     for (char ch : program)
@@ -47,63 +58,88 @@ void compile(std::string program)
         {
         case '>':
         {
-            Value *currentIndex = Builder->CreateLoad(Type::getInt8Ty(*TheContext), tapeIndex, "currentIndex");
-            Value *newVal = Builder->CreateAdd(currentIndex, ConstantInt::get(Type::getInt8Ty(*TheContext), 1), "newVal");
+            Value* currentIndex = getTapeIndex(tapeIndex);
+            Value* newVal = Builder->CreateAdd(currentIndex, ConstantInt::get(Type::getInt32Ty(*TheContext), 1), "newVal");
             Builder->CreateStore(newVal, tapeIndex);
             break;
         }
         case '<':
         {
-            Value *currentIndex = Builder->CreateLoad(Type::getInt8Ty(*TheContext), tapeIndex, "currentIndex");
-            Value *newVal = Builder->CreateSub(currentIndex, ConstantInt::get(Type::getInt8Ty(*TheContext), 1), "newVal");
+            Value* currentIndex = getTapeIndex(tapeIndex);
+            Value* newVal = Builder->CreateSub(currentIndex, ConstantInt::get(Type::getInt32Ty(*TheContext), 1), "newVal");
             Builder->CreateStore(newVal, tapeIndex);
             break;
         }
         case '+':
         {
-            Value *currentIndex = Builder->CreateLoad(Type::getInt8Ty(*TheContext), tapeIndex, "currentIndex");
-            Value *ptr = Builder->CreateGEP(TapeType, Tape, {ConstantInt::get(Type::getInt8Ty(*TheContext), 0), currentIndex}, "tapePtr"); // ptr = TAPE + IDX
-            Value *currentVal = Builder->CreateLoad(Type::getInt8Ty(*TheContext), ptr, "currentVal");
-            Value *newVal = Builder->CreateAdd(currentVal, ConstantInt::get(Type::getInt8Ty(*TheContext), 1), "newVal");
+            Value* currentIndex = getTapeIndex(tapeIndex);
+            Value* ptr = getTapePtr(TapeType, Tape, currentIndex);
+            Value* currentVal = Builder->CreateLoad(Type::getInt8Ty(*TheContext), ptr, "currentVal");
+            Value* newVal = Builder->CreateAdd(currentVal, ConstantInt::get(Type::getInt8Ty(*TheContext), 1), "newVal");
             Builder->CreateStore(newVal, ptr);
             break;
         }
         case '-':
         {
-            Value *currentIndex = Builder->CreateLoad(Type::getInt8Ty(*TheContext), tapeIndex, "currentIndex");
-            Value *ptr = Builder->CreateGEP(TapeType, Tape, {ConstantInt::get(Type::getInt8Ty(*TheContext), 0), currentIndex}, "tapePtr");
-            Value *currentVal = Builder->CreateLoad(Type::getInt8Ty(*TheContext), ptr, "currentVal");
-            Value *newVal = Builder->CreateSub(currentVal, ConstantInt::get(Type::getInt8Ty(*TheContext), 1), "newVal");
+            Value* currentIndex = getTapeIndex(tapeIndex);
+            Value* ptr = getTapePtr(TapeType, Tape, currentIndex);
+            Value* currentVal = Builder->CreateLoad(Type::getInt8Ty(*TheContext), ptr, "currentVal");
+            Value* newVal = Builder->CreateSub(currentVal, ConstantInt::get(Type::getInt8Ty(*TheContext), 1), "newVal");
             Builder->CreateStore(newVal, ptr);
             break;
         }
         break;
         case '.':
         {
-            Value *currentIndex = Builder->CreateLoad(Type::getInt8Ty(*TheContext), tapeIndex, "currentIndex");
-            Value *ptr = Builder->CreateGEP(TapeType, Tape, {ConstantInt::get(Type::getInt8Ty(*TheContext), 0), currentIndex}, "tapePtr");
-            Value *currentVal = Builder->CreateLoad(Type::getInt8Ty(*TheContext), ptr, "currentVal");
+            Value* currentIndex = getTapeIndex(tapeIndex);
+            Value* ptr = getTapePtr(TapeType, Tape, currentIndex);
+            Value* currentVal = Builder->CreateLoad(Type::getInt8Ty(*TheContext), ptr, "currentVal");
             Builder->CreateCall(putcharFunc, currentVal);
             break;
         }
         case ',':
         {
-            Value *inputVal = Builder->CreateCall(getcharFunc);
-            Value *currentIndex = Builder->CreateLoad(Type::getInt8Ty(*TheContext), tapeIndex, "currentIndex");
-            Value *ptr = Builder->CreateGEP(TapeType, Tape, {ConstantInt::get(Type::getInt8Ty(*TheContext), 0), currentIndex}, "tapePtr"); // ptr = TAPE + IDX
+            Value* inputVal = Builder->CreateCall(getcharFunc);
+            Value* currentIndex = Builder->CreateLoad(Type::getInt8Ty(*TheContext), tapeIndex, "currentIndex");
+            Value* ptr = getTapePtr(TapeType, Tape, currentIndex);
             Builder->CreateStore(inputVal, ptr);
             break;
         }
         case '[':
-            // asm_b.Add_instr("cmp     BYTE [%s], 0", TAPE_PTR)
-            // asm_b.Add_instr("je      right_%s", strconv.Itoa(bracket_pairs[i]))
-            // asm_b.Add_label("left_%s", strconv.Itoa(i))
+        {
+            BasicBlock* loopCond = BasicBlock::Create(*TheContext, "loopCond", mainFunction);
+            BasicBlock* loopBody = BasicBlock::Create(*TheContext, "loopBody", mainFunction);
+            BasicBlock* afterLoop = BasicBlock::Create(*TheContext, "afterLoop", mainFunction);
+            Builder->CreateBr(loopCond);
+            Builder->SetInsertPoint(loopCond);
+            
+            Value* currentIndex = getTapeIndex(tapeIndex);
+            Value* ptr = getTapePtr(TapeType, Tape, currentIndex);
+            Value* cellValue = Builder->CreateLoad(Type::getInt8Ty(*TheContext), ptr, "cellValue");
+            Value* isZero = Builder->CreateICmpEQ(cellValue, ConstantInt::get(Type::getInt8Ty(*TheContext), 0), "isZero");
+            Builder->CreateCondBr(isZero, afterLoop, loopBody);
+            Builder->SetInsertPoint(loopBody);
+            loopStack.push({loopCond, afterLoop});
             break;
+        }
         case ']':
-            // asm_b.Add_instr("cmp     BYTE [%s], 0", TAPE_PTR)
-            // asm_b.Add_instr("jne     left_%s", strconv.Itoa(bracket_pairs[i]))
-            // asm_b.Add_label("right_%s", strconv.Itoa(i))
+        {
+            auto pair = loopStack.top();
+            auto loopCond = pair.first;
+            auto afterLoop = pair.second;
+            loopStack.pop();
+
+            // Value* currentIndex = Builder->CreateLoad(Type::getInt8Ty(*TheContext), tapeIndex, "currentIndex");
+            // Value* ptr = Builder->CreateGEP(TapeType, Tape, {ConstantInt::get(Type::getInt8Ty(*TheContext), 0), currentIndex}, "tapePtr");
+            // Value* cellValue = Builder->CreateLoad(Type::getInt8Ty(*TheContext), ptr, "cellValue");
+            // Value* isNonZero = Builder->CreateICmpNE(cellValue, ConstantInt::get(Type::getInt8Ty(*TheContext), 0), "isNonZero");
+            
+            // Builder->CreateCondBr(isNonZero, loopCond, afterLoop);
+            Builder->CreateBr(loopCond);
+            Builder->SetInsertPoint(afterLoop);
             break;
+        }
+
         default:
             break; // do nothing
         }
